df8433b2c58091fdf55e0f4f363e73b274653875
nottnottloop <6772127+nottnottloop@users.noreply.github.com>
2021-09-08 11:31:57 +0100

    feat: increase scale size

--- src/main.cpp
+++ src/main.cpp
@@ -30,7 +30,7 @@ int main(int argc, char* argv[]) {
 		cells[i].reserve(8);
 	}
 
-	constexpr float CELL_SCALE = 0.7f;
+	constexpr float CELL_SCALE = 0.8f;
 	constexpr float CENTER_X = (SCREEN_WIDTH / 2) - 64.0f * CELL_SCALE * 4;
 	constexpr float CENTER_Y = (SCREEN_HEIGHT / 2) - 64.0f * CELL_SCALE * 4;
 	for (int i = 0; i < 8; i++) {
