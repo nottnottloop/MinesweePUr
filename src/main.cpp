db80b697e47adebdcaac9b627fc534d842cadc82
nottnottloop <6772127+nottnottloop@users.noreply.github.com>
2021-09-09 23:36:51 +0100

    refactor: switchLevel function. this is when the code became serious spaghetti

--- src/main.cpp
+++ src/main.cpp
@@ -30,6 +30,63 @@ void checkButtonClick(Sint32 x, Sint32 y, bool right_mouse) {
 	}
 }
 
+SDL_Color red = {255, 0, 0};
+SDL_Color green = {0, 255, 0};
+SDL_Color blue = {0, 0, 255};
+SDL_Color cyan = {0, 255, 255};
+SDL_Color white = {255, 255, 255};
+SDL_Color black = {0, 0, 0};
+
+void switchLevel(int level, Game& game, Text& text, Button& restart_button) {
+	switch (level) {
+		case 1:
+			text.loadFontTexture(FONT_LOCATION, FONT_SIZE, green, "Beginner");
+			text.setOffset({0, 0});
+			game.clearBoard();
+			#ifdef DEBUG_MINES
+				game.setBoard(8, 8, DEBUG_MINES);
+			#else
+				game.setBoard(8, 8, 10);
+			#endif
+			restart_button.setOffset({0, 15});
+			game.setCellScale(1.0f);
+			game.setOffset({0, BEGINNER_OFFSET});
+			game.initBoard();
+			game.generateBoard();
+			break;
+		case 2:
+			text.loadFontTexture(FONT_LOCATION, FONT_SIZE, cyan, "Medium");
+			text.setOffset({0, -10});
+			game.clearBoard();
+			#ifdef DEBUG_MINES
+				game.setBoard(16, 16, DEBUG_MINES);
+			#else
+				game.setBoard(16, 16, 40);
+			#endif
+			restart_button.setOffset({0, 5});
+			game.setCellScale(0.6f);
+			game.setOffset({0, MEDIUM_OFFSET});
+			game.initBoard();
+			game.generateBoard();
+			break;
+		case 3:
+			text.loadFontTexture(FONT_LOCATION, FONT_SIZE, red, "Expert");
+			text.setOffset({0, -10});
+			game.clearBoard();
+			#ifdef DEBUG_MINES
+				game.setBoard(16, 30, DEBUG_MINES);
+			#else
+				game.setBoard(16, 30, 99);
+			#endif
+			restart_button.setOffset({0, 5});
+			game.setCellScale(0.5f);
+			game.setOffset({0, EXPERT_OFFSET});
+			game.initBoard();
+			game.generateBoard();
+			break;
+	}
+}
+
 //system variables
 //int SCREEN_WIDTH = 640;
 //int SCREEN_HEIGHT = 480;
@@ -45,12 +102,6 @@ SDL_Texture* fg = window.loadTexture("res/fg.png");
 SDL_Texture* awesome = window.loadTexture("res/awesome.png");
 SDL_Texture* vol = window.loadTexture("res/vol.png");
 
-SDL_Color red = {255, 0, 0};
-SDL_Color green = {0, 255, 0};
-SDL_Color blue = {0, 0, 255};
-SDL_Color cyan = {0, 255, 255};
-SDL_Color white = {255, 255, 255};
-SDL_Color black = {0, 0, 0};
 Text text({650, 40}, {0, 0});
 
 Mix_Chunk* click;
@@ -89,6 +140,15 @@ int main(int argc, char* argv[]) {
 	Button mute_button({SCREEN_WIDTH - 128.0f, SCREEN_HEIGHT - 128.0f}, {0, 0}, {0, 0, 128, 128}, {0, 0, 128, 128}, nullptr, vol, game, &Game::toggleMute);
 	buttons.push_back(&mute_button);
 
+	Button oneBut({0, 0}, {0, 0}, {0, 0, 60, 60}, {0, 0, 60, 60}, bg, fg, game, &Game::toggleMute);
+	buttons.push_back(&oneBut);
+
+	Button twoBut({60, 0}, {0, 0}, {0, 0, 60, 60}, {60, 0, 60, 60}, bg, fg, game, &Game::toggleMute);
+	buttons.push_back(&twoBut);
+
+	Button threeBut({120, 0}, {0, 0}, {0, 0, 60, 60}, {120, 0, 60, 60}, bg, fg, game, &Game::toggleMute);
+	buttons.push_back(&threeBut);
+
 	click = Mix_LoadWAV("res/click.ogg");
 	kaboom = Mix_LoadWAV("res/kaboom.ogg");
 
@@ -125,49 +185,13 @@ int main(int argc, char* argv[]) {
 							game.restart();
 							break;
 						case SDLK_1:
-							text.loadFontTexture(FONT_LOCATION, FONT_SIZE, green, "Beginner");
-							text.setOffset({0, 0});
-							game.clearBoard();
-							#ifdef DEBUG_MINES
-								game.setBoard(8, 8, DEBUG_MINES);
-							#else
-								game.setBoard(8, 8, 10);
-							#endif
-							restart_button.setOffset({0, 15});
-							game.setCellScale(1.0f);
-							game.setOffset({0, BEGINNER_OFFSET});
-							game.initBoard();
-							game.generateBoard();
+							switchLevel(1, game, text, restart_button);
 							break;
 						case SDLK_2:
-							text.loadFontTexture(FONT_LOCATION, FONT_SIZE, cyan, "Medium");
-							text.setOffset({0, -10});
-							game.clearBoard();
-							#ifdef DEBUG_MINES
-								game.setBoard(16, 16, DEBUG_MINES);
-							#else
-								game.setBoard(16, 16, 40);
-							#endif
-							restart_button.setOffset({0, 5});
-							game.setCellScale(0.6f);
-							game.setOffset({0, MEDIUM_OFFSET});
-							game.initBoard();
-							game.generateBoard();
+							switchLevel(2, game, text, restart_button);
 							break;
 						case SDLK_3:
-							text.loadFontTexture(FONT_LOCATION, FONT_SIZE, red, "Expert");
-							text.setOffset({0, -10});
-							game.clearBoard();
-							#ifdef DEBUG_MINES
-								game.setBoard(16, 30, DEBUG_MINES);
-							#else
-								game.setBoard(16, 30, 99);
-							#endif
-							restart_button.setOffset({0, 5});
-							game.setCellScale(0.5f);
-							game.setOffset({0, EXPERT_OFFSET});
-							game.initBoard();
-							game.generateBoard();
+							switchLevel(3, game, text, restart_button);
 							break;
 					}
 			}
